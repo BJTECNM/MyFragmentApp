83d451adf670298243a3ca4584b3a60f2c35a480
BJ <192t0220@itsm.edu.mx>
2023-10-14 17:57:43 -0600

    git commit

--- app/src/main/java/com/example/myfragmentapp/DatosFragment.kt
+++ app/src/main/java/com/example/myfragmentapp/DatosFragment.kt
@@ -11,7 +11,7 @@ class DatosFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_datos, container, false)
         return view
